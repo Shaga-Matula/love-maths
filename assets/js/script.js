1daaf8acc32f826f3c129b5e050fa1b29a6652c8
Paul-Gleeson <Shagamatula@gmail.com>
2023-02-16 14:45:41 +0000

    add event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,53 @@
-function runGame(){
+    document.addEventListener("DOMContentLoaded", function () {
+            let buttons = document.getElementsByTagName("button");
+            
+            for (let button of buttons) {
+                
+               button.addEventListener("click", function () {
+                    if (this.getAttribute("data-type") === "submit") {
+                        alert("You clicked submit!");
+                    } else {
+                      let gameType = this.getAttribute("data-type");
+                      alert(`You clicked ${gameType}`);  
+                    }
+                } );
+            }
+        });
 
-}
 
-function checkAnswer(){
-
-}
+        
 
 
-function calculateCorrectAnswer(){
+        function runGame() {
 
-}
+        }
+
+        function checkAnswer() {
+
+        }
 
 
-function incrementScore(){
+        function calculateCorrectAnswer() {
 
-}
+        }
 
-function incrementWrongAnswer(){
 
-}
+        function incrementScore() {
 
-function displayAdditionQuestion(){
+        }
 
-}
+        function incrementWrongAnswer() {
 
-function displaySubtractQuestion(){
+        }
 
-}
+        function displayAdditionQuestion() {
 
-function displayMultiplyQuestion(){
+        }
 
-}
+        function displaySubtractQuestion() {
+
+        }
+
+        function displayMultiplyQuestion() {
+
+        }
