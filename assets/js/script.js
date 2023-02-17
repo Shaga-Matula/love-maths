a4d9db56b1f291565537c103f48baca64c56af3a
Paul-Gleeson <Shagamatula@gmail.com>
2023-02-17 14:58:00 +0000

    add division script

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,8 +15,8 @@
 
 
         }
-        document.getElementById("answer-box").addEventListener("keydown", function(event){
-            if (event.key === "Enter" ){
+        document.getElementById("answer-box").addEventListener("keydown", function (event) {
+            if (event.key === "Enter") {
                 checkAnswer();
             }
 
@@ -46,6 +46,12 @@
 
         } else if (gameType === "subtract") {
             displaySubtractQuestion(num1, num2);
+        
+        } else if (gameType === "division") {
+            num2 = Math.floor(Math.random() * 300) + 1;
+            alert(`operand2 level is  ${operand2} `);
+            displayDivisionQuestion(num1, num2);
+        
         } else {
             alert(`Unknown Game Type ${gameType}`);
             throw `Unknown Game Type ${gameType}.Aborting!`;
@@ -89,6 +95,8 @@
             return [operand1 * operand2, "multiply"];
         } else if (operator === "-") {
             return [operand1 - operand2, "subtract"];
+        } else if (operator === "/") {
+           return [operand1 / operand2, "division"];
         } else {
             alert(`Unimplamented Operator Type ${operator}`);
             throw `Unimplamented Operator Type ${operator}.Aborting!`;
@@ -128,5 +136,12 @@
         document.getElementById('operand1').textContent = operand1;
         document.getElementById('operand2').textContent = operand2;
         document.getElementById('operator').textContent = "x";
+
+    }
+
+    function displayDivisionQuestion(operand1, operand2) {
+        document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;;
+        document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;;
+        document.getElementById('operator').textContent = "/";
 
     }
